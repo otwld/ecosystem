9ff558a3b671552a9bb68485034b317ac3fc55c2
TREHOUT Nathan <ntrehout@outworld.fr>
2022-11-10 19:05:51 +0800

    👷 (.drone.yml): corrige & refacto les erreurs de paths, naming, etc...

--- projects/apps/api-otwld-fr/jest.config.ts
+++ projects/apps/api-otwld-fr/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'api-otwld-fr',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
--- projects/apps/otwld-fr/jest.config.ts
+++ projects/apps/otwld-fr/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'otwld-fr',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
