4a17d0640d31019219729ee464c0984da926459a
TREHOUT Nathan <ntrehout@outworld.fr>
2022-11-25 20:13:01 +0800

    ✨ (libs-ui): directive base

---
+++ libs/ui/src/directives/base.directive.ts
@@ -0,0 +1,41 @@
+import {
+  Directive,
+  HostBinding,
+  Input,
+  OnChanges,
+  OnDestroy,
+} from '@angular/core';
+import { ThemeClassOrArray } from '../types/tailwind.types';
+import { constructComponentClasses } from '../utils/tailwind.utils';
+import { UntilDestroy } from '@ngneat/until-destroy';
+
+@UntilDestroy()
+@Directive()
+export class BaseDirective<T extends string = ''>
+  implements OnChanges, OnDestroy
+{
+  class = '';
+
+  ngOnChanges(): void {
+    this.class = this.construct(this.componentBase, this.componentModifiers);
+  }
+
+  protected construct(
+    componentBase: () => ThemeClassOrArray,
+    componentModifiers: () => ThemeClassOrArray[]
+  ) {
+    this.componentBase = componentBase;
+    this.componentModifiers = componentModifiers;
+    return constructComponentClasses(
+      this.componentBase(),
+      this.componentModifiers()
+    );
+  }
+
+  private componentBase: () => ThemeClassOrArray = () => [];
+
+  private componentModifiers: () => ThemeClassOrArray[] = () => [];
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method,@typescript-eslint/no-empty-function
+  ngOnDestroy(): void {}
+}
