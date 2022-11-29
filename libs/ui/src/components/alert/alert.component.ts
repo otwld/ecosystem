4ca9716acbac0553880b4b0a53e7afe45d266047
TREHOUT Nathan <ntrehout@outworld.fr>
2022-11-29 19:14:57 +0800

    ✨ (libs-ui): composant AlertComponent & AlertActionsComponent

---
+++ libs/ui/src/components/alert/alert.component.ts
@@ -0,0 +1,63 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input, NgModule,
+  TemplateRef
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertColorClass } from '../../types/tailwind/components/alert.types';
+import { BaseComponent } from '../base.component';
+import {
+  faCheckCircle,
+  faClose,
+  faInfoCircle,
+  faWarning,
+} from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'ui-alert',
+  templateUrl: './alert.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AlertComponent extends BaseComponent {
+  @Input() color: AlertColorClass | undefined = undefined;
+  @HostBinding('class') override class = this.construct(
+    () => ['alert'],
+    () => [this.color]
+  );
+  @Input() title: string | undefined = undefined;
+  @Input() message: string | undefined = undefined;
+  @Input() slotEndTpl: TemplateRef<unknown> | undefined = undefined;
+  @Input() icon: IconDefinition | undefined = undefined;
+
+  config: Record<AlertColorClass | 'default', { icon: IconDefinition }> = {
+    'alert-info': { icon: faInfoCircle },
+    'alert-success': { icon: faCheckCircle },
+    'alert-warning': { icon: faWarning },
+    'alert-error': { icon: faClose },
+    default: { icon: faInfoCircle },
+  };
+}
+
+@Component({
+  selector: 'ui-alert-actions',
+  template: `<ng-content></ng-content>`,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AlertActionsComponent extends BaseComponent {
+  @HostBinding('class') override class = this.construct(
+    () => ['flex-none']
+  );
+}
+
+@NgModule({
+  imports: [CommonModule, FontAwesomeModule],
+  declarations: [AlertComponent, AlertActionsComponent],
+  exports: [AlertComponent, AlertActionsComponent],
+})
+export class AlertModule {}
