22c2c715e70d1efe21ef83ca5682f6dab4e94616
TREHOUT Nathan <ntrehout@outworld.fr>
2022-11-30 16:33:33 +0800

    🏷️️ (libs-ui): ajout de mask.types.ts

---
+++ libs/ui/src/types/tailwind/components/mask.types.ts
@@ -0,0 +1,24 @@
+type MaskBaseClass = 'mask';
+export type MaskTypeClass =
+  | 'mask-squircle'
+  | 'mask-heart'
+  | 'mask-hexagon'
+  | 'mask-hexagon-2'
+  | 'mask-decagon'
+  | 'mask-pentagon'
+  | 'mask-diamond'
+  | 'mask-square'
+  | 'mask-circle'
+  | 'mask-parallelogram'
+  | 'mask-parallelogram-2'
+  | 'mask-parallelogram-3'
+  | 'mask-parallelogram-4'
+  | 'mask-star'
+  | 'mask-star-2'
+  | 'mask-triangle'
+  | 'mask-triangle-2'
+  | 'mask-triangle-3'
+  | 'mask-triangle-4'
+  | 'mask-half-1'
+  | 'mask-half-2';
+export type MaskClass = MaskBaseClass | MaskTypeClass;
