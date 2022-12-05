98c5c94b12717acf309b99807726a53ec28581a7
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-05 19:16:47 +0100

    ✨ (otwld-fr/main.ts): configure le mode production de transloco

--- projects/apps/otwld-fr/src/main.ts
+++ projects/apps/otwld-fr/src/main.ts
@@ -4,10 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { enableElfProdMode } from '@ngneat/elf';
+import { translocoConfig } from '@ngneat/transloco';
 
 if (environment.production) {
   enableProdMode();
   enableElfProdMode();
+  translocoConfig({
+    prodMode: true
+  })
 }
 
 platformBrowserDynamic()
