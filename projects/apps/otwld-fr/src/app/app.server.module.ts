a5fe045a639c0a8c05e077ae5dca60824ceaf8cf
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-06 18:17:14 +0100

    ♻️ (otwld-fr/app.server.module): supprime le code inutile

--- projects/apps/otwld-fr/src/app/app.server.module.ts
+++ projects/apps/otwld-fr/src/app/app.server.module.ts
@@ -3,16 +3,9 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { DateFnsConfigurationService } from 'ngx-date-fns';
-import { enUS } from 'date-fns/locale';
 
-const dateFnsConfig = new DateFnsConfigurationService();
-dateFnsConfig.setLocale(enUS);
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
+  imports: [AppModule, ServerModule],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
