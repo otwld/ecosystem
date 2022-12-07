a364ee723b38dcf1aa2ab44fc7c21a2f9162419a
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-07 17:37:05 +0100

    🍻 last version angular & nx, problème de DepGraph vide présent, on attend le fix

--- projects/apps/otwld-fr/tailwind.config.js
+++ projects/apps/otwld-fr/tailwind.config.js
@@ -2,10 +2,20 @@ const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
 const sharedTailwindConfig = require('../../../libs/ui/tailwind.config');
 
+/**
+ * node_modules/@nrwl/workspace/src/utilities/generate-globs.js#28
+ *
+ *    ie: const filenameRelativeToWorkspaceRoot = (0, path_1.relative)(workspace_root_1.workspaceRoot, dirPath);
+ *
+ *    Correction: const filenameRelativeToWorkspaceRoot = (0, path_1.relative)(workspace_root_1.workspaceRoot, dirPath).replace(/\\/g, '/');
+ *    Error: the path is in windows format, so the Map<> can't be accessed.
+ */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, '../../../libs/ui/src/**/!(*.stories|*.spec).{ts,html}'), // IE: We are waiting for NX to fix this issue.
     ...createGlobPatternsForDependencies(__dirname),
   ],
   presets: [sharedTailwindConfig],
