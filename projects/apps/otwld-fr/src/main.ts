22509045885a420506204413ed7f6b6991966c5c
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-08 16:36:10 +0100

    ✨ (otwld-fr/server): configure transloco & elf en fonction de production

--- projects/apps/otwld-fr/src/main.ts
+++ projects/apps/otwld-fr/src/main.ts
@@ -3,9 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { translocoConfig } from '@ngneat/transloco';
+import { enableElfProdMode } from '@ngneat/elf';
 
 if (environment.production) {
   enableProdMode();
+  translocoConfig(
+    {
+      prodMode: true,
+    }
+  )
+  enableElfProdMode();
 }
 
 function bootstrap() {
