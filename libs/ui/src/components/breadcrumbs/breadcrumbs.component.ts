df881f4b44561d1c406d04a3b7baa53f8f0ef1ea
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-09 15:53:55 +0100

    ♻️ (libs-ui/breadcrumbs): refacto + export du type

--- libs/ui/src/components/breadcrumbs/breadcrumbs.component.ts
+++ libs/ui/src/components/breadcrumbs/breadcrumbs.component.ts
@@ -7,13 +7,26 @@ import {
 import { CommonModule } from '@angular/common';
 import { BaseComponent } from '../base.component';
 import { Icon } from '@fortawesome/fontawesome-svg-core';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslocoModule } from '@ngneat/transloco';
+
+export interface Breadcrumb {
+  labelTranslationKey: string;
+  url?: string;
+  icon?: Icon;
+}
 
 @Component({
   selector: 'ui-breadcrumbs',
   standalone: true,
-  imports: [CommonModule, RouterLinkWithHref, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    RouterLinkWithHref,
+    FontAwesomeModule,
+    RouterLink,
+    TranslocoModule,
+  ],
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,6 +37,5 @@ export class BreadcrumbsComponent extends BaseComponent {
     () => []
   );
 
-  @Input() items: { labelTranslationKey: string; url?: string; icon?: Icon }[] =
-    [];
+  @Input() items: Breadcrumb[] = [];
 }
