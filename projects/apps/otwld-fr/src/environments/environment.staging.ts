959382ec8047b04cc9cbd4df758fd6b6bf1834f4
TREHOUT Nathan <ntrehout@outworld.fr>
2022-12-21 00:59:35 +0100

    🔧 (otwld-fr): ajoute les configurations development, staging, local et production

---
+++ projects/apps/otwld-fr/src/environments/environment.staging.ts
@@ -0,0 +1,59 @@
+export const environment = {
+  baseUrl: 'http://outworld.staging',
+  production: true,
+  modules: [],
+  templates: {
+    services: {
+      'desktop-development': {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/en/desktop-development.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/fr/desktop-development.html',
+      },
+      'mobile-development': {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/en/mobile-development.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/fr/mobile-development.html',
+      },
+      'product-development': {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/en/product-development.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/fr/product-development.html',
+      },
+      'team-extension': {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/en/team-extension.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/fr/team-extension.html',
+      },
+      'web-development': {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/en/web-development.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/services/fr/web-development.html',
+      },
+    },
+    portfolio: {
+      occ: {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/en/occ.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/fr/occ.html',
+      },
+      jeprecommande: {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/en/jeprecommande.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/fr/jeprecommande.html',
+      },
+      onstage: {
+        enURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/en/onstage.html',
+        frURL:
+          'https://raw.githubusercontent.com/otwld/ecosystem/master/projects/apps/otwld-fr/src/assets/static/portfolio/fr/onstage.html',
+      },
+    },
+  },
+};
