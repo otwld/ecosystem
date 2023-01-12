184d7c7b7435260a67a45f402080d6710a9af777
apain-dev <a.pain@mesdocteurs.com>
2023-01-12 14:32:30 +0100

    fix(api-otwld-fr/env): change keys for environment variables

--- projects/apps/api-otwld-fr/src/config/mongodb.config.ts
+++ projects/apps/api-otwld-fr/src/config/mongodb.config.ts
@@ -2,12 +2,12 @@ import {registerAs} from '@nestjs/config';
 
 export default registerAs('mongodb', () => {
   const option = {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10),
-    name: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    pwd: process.env.DATABASE_PASSWORD,
-    uri: process.env.DATABASE_URI,
+    host: process.env.MONGODB_HOST,
+    port: parseInt(process.env.MONGODB_PORT, 10),
+    name: process.env.MONGODB_NAME,
+    user: process.env.MONGODB_USER,
+    pwd: process.env.MONGODB_PASSWORD,
+    uri: process.env.MONGODB_URI,
   };
   return {
     ...option,
