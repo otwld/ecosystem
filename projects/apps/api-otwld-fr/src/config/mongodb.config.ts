8f6aa8a92736866ba424f6e4aa09e88e82716048
apain-dev <a.pain@mesdocteurs.com>
2023-01-12 15:11:14 +0100

    fix(charts/api-otwld-fr): fix MONGO_USERNAME MONGODB_DATABSE url

--- projects/apps/api-otwld-fr/src/config/mongodb.config.ts
+++ projects/apps/api-otwld-fr/src/config/mongodb.config.ts
@@ -4,8 +4,8 @@ export default registerAs('mongodb', () => {
   const option = {
     host: process.env.MONGODB_HOST,
     port: parseInt(process.env.MONGODB_PORT, 10),
-    name: process.env.MONGODB_NAME,
-    user: process.env.MONGODB_USER,
+    name: process.env.MONGODB_DATABASE,
+    user: process.env.MONGODB_USERNAME,
     pwd: process.env.MONGODB_PASSWORD,
     uri: process.env.MONGODB_URI,
   };
