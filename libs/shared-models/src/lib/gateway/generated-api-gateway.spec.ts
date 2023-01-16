f346f6d63fe65f81e9d70ad1ddd5457421b17918
apain-dev <a.pain@mesdocteurs.com>
2023-01-16 18:47:27 +0100

    test(libs/shared-models): add test for api gateway

---
+++ libs/shared-models/src/lib/gateway/generated-api-gateway.spec.ts
@@ -0,0 +1,7 @@
+import {GetServicesPaginatedForHomeGQL} from './generated-api-gateway';
+
+describe('GeneratedApiGateway', () => {
+  it('should create', () => {
+    expect(GetServicesPaginatedForHomeGQL).toBeTruthy();
+  });
+});
