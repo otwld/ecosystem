9cf8a4754e102f3f94373291c772704c06954334
apain-dev <arthur.pain@epitech.eu>
2023-01-22 20:26:16 +0000

    feat(otwld-fr/environment): change url of api for development

--- projects/apps/otwld-fr/src/environments/environment.development.ts
+++ projects/apps/otwld-fr/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'https://development.outworld.fr',
-  apiUrl: 'http://localhost:3000/graphql',
+  apiUrl: 'https://development-api.outworld.fr/graphql',
   appName: 'otwld-fr',
   production: true,
   modules: [],
