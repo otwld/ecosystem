58e61a413a376e0592526f9a14a1f0e3261be7d4
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-25 19:48:41 +0100

    🚨 (api-otwld-fr/multiple-id.service): remove @ts-ignore and comment out the whole code with error TS1206

--- projects/apps/api-otwld-fr/src/app/shared/modules/language/models/abstractResolver.traduction.ts
+++ projects/apps/api-otwld-fr/src/app/shared/modules/language/models/abstractResolver.traduction.ts
@@ -1,10 +1,9 @@
-import {Parent} from '@nestjs/graphql';
-import {CurrentLanguage} from '../decorators/current-language.decorator';
-import {HeaderLanguage} from '../enums/language.enum';
+// import {Parent} from '@nestjs/graphql';
+// import {CurrentLanguage} from '../decorators/current-language.decorator';
+// import {HeaderLanguage} from '../enums/language.enum';
 
 export function createTranslationResolver<T>(field: string) {
-// @ts-ignore
-  return function translationResolver(@Parent() parent: T, @CurrentLanguage() language: HeaderLanguage) {
-    return parent[field][language];
-  };
+  // return function translationResolver(@Parent() parent: T, @CurrentLanguage() language: HeaderLanguage) {
+  //   return parent[field][language];
+  // };
 }
