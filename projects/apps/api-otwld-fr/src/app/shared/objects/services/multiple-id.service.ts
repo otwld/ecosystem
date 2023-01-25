eee811c9b221112337961156efdfd4fc6f3b7d6e
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-25 19:48:15 +0100

    🚨 (api-otwld-fr/multiple-id.service): remove @ts-ignore and replace with hotfix (as unknown as T)

--- projects/apps/api-otwld-fr/src/app/shared/objects/services/multiple-id.service.ts
+++ projects/apps/api-otwld-fr/src/app/shared/objects/services/multiple-id.service.ts
@@ -1,17 +1,17 @@
 import { AppLogger } from '../../modules/logging/logging.service';
-import {FilterQuery, Model} from 'mongoose';
-import { Document } from 'mongoose';
+import { Document, FilterQuery, Model } from 'mongoose';
 
 export class GetMultipleIds<T> {
-  constructor(protected loader: AppLogger, protected model: Model<T & Document>) {}
+  constructor(
+    protected loader: AppLogger,
+    protected model: Model<T & Document>
+  ) {}
 
   getMultipleByIds(ids: string[]): Promise<T[]> {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     return this.model
       .find({ _id: { $in: ids } })
       .lean()
-      .exec();
+      .exec() as unknown as Promise<T[]>;
   }
 
   getOneByFilter(filter: FilterQuery<T & Document>) {
