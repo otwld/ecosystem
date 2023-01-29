5896b41b5edcacebd6a03dc7cf9c03d3bf699694
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-29 17:24:57 +0100

    🔧 (otwld-fr/environment.*): correct the apiURL

--- projects/apps/otwld-fr/src/environments/environment.staging.ts
+++ projects/apps/otwld-fr/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'https://staging.outworld.fr',
-  apiUrl: 'http://localhost:3000/graphql',
+  apiUrl: 'https://staging-api.outworld.fr/graphql',
   appName: 'otwld-fr',
   production: true,
   modules: [],
