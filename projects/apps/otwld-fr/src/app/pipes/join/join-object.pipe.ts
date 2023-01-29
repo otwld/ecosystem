ce6018392c9f0e63f027bf648068a369140e20a4
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-29 16:31:19 +0100

    💥 (otwld-fr/join-object.pipe): now by default not translating, but you can pass an argument to enable translation

--- projects/apps/otwld-fr/src/app/pipes/join/join-object.pipe.ts
+++ projects/apps/otwld-fr/src/app/pipes/join/join-object.pipe.ts
@@ -5,17 +5,24 @@ import { TranslocoService } from '@ngneat/transloco';
 @Pipe({
   name: 'joinObject',
   standalone: true,
-  pure: false
+  pure: false,
 })
 export class JoinObjectPipe implements PipeTransform {
   private join = new JoinPipe().transform;
   private translocoService = inject(TranslocoService);
 
-  transform<T>(value: T[], key: keyof T, separator = ', '): unknown {
+  transform<T>(
+    value: T[],
+    key: keyof T,
+    separator = ', ',
+    translate = false
+  ): unknown {
     return this.join(
       value.map((v) => {
         if (typeof v[key] === 'string') {
-          return this.translocoService.translate(v[key] as string);
+          return translate
+            ? this.translocoService.translate(v[key] as string)
+            : (v[key] as string);
         } else {
           return JSON.stringify(v[key]);
         }
