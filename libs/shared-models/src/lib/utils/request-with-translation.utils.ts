714097859fe6c98b34f9ecdacf326d5c8e00455a
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-29 16:29:30 +0100

    🚑 (libs-shared-models/request-with-translation.utils): add a temporary delay(0) while waiting to find a proper solution to handle langChanges propagation (Interceptor / Logic)

--- libs/shared-models/src/lib/utils/request-with-translation.utils.ts
+++ libs/shared-models/src/lib/utils/request-with-translation.utils.ts
@@ -1,11 +1,14 @@
-import {TranslocoService} from '@ngneat/transloco';
-import {inject} from '@angular/core';
-import {startWith} from 'rxjs';
+import { TranslocoService } from '@ngneat/transloco';
+import { inject } from '@angular/core';
+import { delay, startWith } from 'rxjs';
 
 export class RequestWithTranslationUtils {
   translocoService = inject(TranslocoService);
 
   langChangeUpdate$() {
-    return this.translocoService.langChanges$.pipe(startWith(this.translocoService.getActiveLang()));
+    return this.translocoService.langChanges$.pipe(
+      startWith(this.translocoService.getActiveLang()),
+      delay(0) // TODO(ntrehout): Find a better way to wait for Interceptor to set the lang.
+    );
   }
 }
