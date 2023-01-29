b8c4044b0c99bb970e2ff292691c62618ed17197
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-29 16:27:19 +0100

    ✨ (libs-shared-models/projects.service): extends RequestWithTranslationUtils and wrap request in langChangeUpdate$ / add fetchPolicy: network-only

--- libs/shared-models/src/lib/modules/projects/services/projects.service.ts
+++ libs/shared-models/src/lib/modules/projects/services/projects.service.ts
@@ -1,37 +1,66 @@
-import {inject, Injectable} from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import {
   CarouselProjectFragment,
   GetProjectBySlugGQL,
   GetProjectBySlugQuery,
   GetProjectsPaginatedGQL,
   GetRelatedProjectsGQL,
-  GetRelatedProjectsQuery
+  GetRelatedProjectsQuery,
 } from '../../../gateway/generated-api-gateway';
-import {map, Observable} from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
+import { RequestWithTranslationUtils } from '../../../utils/request-with-translation.utils';
 
-@Injectable({providedIn: 'root'})
-export class ProjectsService {
+@Injectable({ providedIn: 'root' })
+export class ProjectsService extends RequestWithTranslationUtils {
   getProjectBySlugGql = inject(GetProjectBySlugGQL);
   getRelatedProjectsBySlugGql = inject(GetRelatedProjectsGQL);
   getProjectsPaginatedGQL = inject(GetProjectsPaginatedGQL);
 
-  getProjectBySlug$(slug: string): Observable<GetProjectBySlugQuery['getProjectBySlug']> {
-    return this.getProjectBySlugGql.fetch({slug}).pipe(map(({data}) => data.getProjectBySlug))
+  getProjectBySlug$(
+    slug: string
+  ): Observable<GetProjectBySlugQuery['getProjectBySlug']> {
+    return this.langChangeUpdate$().pipe(
+      switchMap(() =>
+        this.getProjectBySlugGql.fetch(
+          { slug },
+          { fetchPolicy: 'network-only' }
+        )
+      ),
+      map(({ data }) => data.getProjectBySlug)
+    );
   }
 
-  getRelatedProjectsBySlug$(slug: string): Observable<GetRelatedProjectsQuery['getRelatedProjects']> {
-    return this.getRelatedProjectsBySlugGql.fetch({slug}).pipe(map(({data}) => data.getRelatedProjects));
+  getRelatedProjectsBySlug$(
+    slug: string
+  ): Observable<GetRelatedProjectsQuery['getRelatedProjects']> {
+    return this.langChangeUpdate$().pipe(
+      switchMap(() =>
+        this.getRelatedProjectsBySlugGql.fetch(
+          { slug },
+          { fetchPolicy: 'network-only' }
+        )
+      ),
+      map(({ data }) => data.getRelatedProjects)
+    );
   }
 
   getPaginatedProjects$(): Observable<CarouselProjectFragment[]> {
-    return this.getProjectsPaginatedGQL.fetch({pagination: {after: 4}})
-      .pipe(map((result) => {
+    return this.langChangeUpdate$().pipe(
+      switchMap(() =>
+        this.getProjectsPaginatedGQL.fetch(
+          { pagination: { after: 4 } },
+          { fetchPolicy: 'network-only' }
+        )
+      ),
+      map((result) => {
         if (result.data.getProjects.edges) {
-          return result.data.getProjects.edges.map((edge) => edge?.node || null)
+          return result.data.getProjects.edges
+            .map((edge) => edge?.node || null)
             .filter((node) => node !== null) as CarouselProjectFragment[];
         } else {
           return [];
         }
-      }));
+      })
+    );
   }
 }
