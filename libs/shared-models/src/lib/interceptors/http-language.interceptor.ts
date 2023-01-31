c73484947cc4800ccd0d19242b0d2496c8a43442
TREHOUT Nathan <ntrehout@outworld.fr>
2023-01-31 11:55:10 +0100

    🐛 (shared-models/http-language.interceptor): use LocalStorageService instead of builtins localStorage

--- libs/shared-models/src/lib/interceptors/http-language.interceptor.ts
+++ libs/shared-models/src/lib/interceptors/http-language.interceptor.ts
@@ -1,14 +1,23 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LocalStorageService } from '@otwld/ui';
 
 @Injectable()
 export class HttpLanguageInterceptor implements HttpInterceptor {
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  private readonly localStorage = inject(LocalStorageService);
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        Language: localStorage?.getItem('lang') || 'en',
+        Language: this.localStorage.getItem('lang') || 'en',
       },
     });
     return next.handle(request);
